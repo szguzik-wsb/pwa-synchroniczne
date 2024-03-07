043115dda0b53f365e14099e9c086ee24d37b859
Arkadiusz Dampc <arekdampc@gmail.com>
2024-03-07 20:58:51 +0100

    added content to footer section

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,11 +1,78 @@
 <template>
     <footer class="footer">
-    <div class="content has-text-centered">
-        <p>
-        <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-        </p>
+    <div class="footer_section">
+        <div class ="footer_hyperlinks">
+            <div class ="first_column">
+                <h3>Zamowienia</h3>
+                <ul class="zamowienia">
+                    <li><a href="#">Dostawa</a></li>
+                    <li><a href="#">Raty</a></li>
+                    <li><a href="#">Leasing</a></li>
+                    <li><a href="#">Ubezpieczenia</a></li>
+                    <li><a href="#">TaxFree</a></li>
+                    <li><a href="#">Montaz</a></li>
+                    <li><a href="#">Zwroty i reklamacje</a></li>
+                    <li><a href="#">Najczesciej zadawane pytania</a></li>
+                </ul>
+                </div>
+                <div class ="second_column">
+                <h3>Promocje i inspiracje</h3>
+                <ul class="promocje">
+                    <li><a href="#">Wyprzedaz</a></li>
+                    <li><a href="#">Goracy strzal</a></li>
+                    <li><a href="#">un.Box</a></li>
+                    <li><a href="#">Promocje</a></li>
+                    <li><a href="#">Karty podarunkowe</a></li>
+                    <li><a href="#">Poradniki</a></li>
+                    <li><a href="#">Aktualnosci</a></li>
+                    </ul>
+                </div>
+                <div class ="third_column">
+                <h3>x-kom</h3>
+                <ul class="x-kom">
+                    <li><a href="#">O nas</a></li>
+                    <li><a href="#">Regulamin</a></li>
+                    <li><a href="#">Polityka prywatnosci</a></li>
+                    <li><a href="#">Polityka cookies</a></li>
+                    <li><a href="#">Regulamin newslettera</a></li>
+                    <li><a href="#">Biuro prasowe</a></li>
+                    <li><a href="#">Zamowienia publiczne</a></li>
+                    <li><a href="#">Zakupy dla firm</a></li>
+                    <li><a href="#">Wspolpraca marketingowa</a></li>
+                    <li><a href="#">Geex</a></li>
+                    <li><a href="#">Forum</a></li>
+                    <li><a href="#">Kariera</a></li>
+                    <li><a href="#">Kontakt</a></li>
+                    <li><a href="#">Realizowane projekty</a></li>
+                    </ul>
+                </div>
+        </div>
+        <div class="contact">
+            <div class="contact_info">
+                <h3>Kontakt</h3>
+                <p>Telefon: 123 456 789</p>
+                <p>Email:</p>
+                <div class="social_media">
+                    <a href="#"><img src="https://cdn.iconscout.com/icon/free/png-256/facebook-185-498898.png" alt="facebook"></a>
+                    <a href="#"><img src="https://cdn.iconscout.com/icon/free/png-256/instagram-233-896451.png" alt="instagram"></a>
+                    <a href="#"><img src="https://cdn.iconscout.com/icon/free/png-256/twitter-1867541-1583140.png" alt="twitter"></a>
+                    <a href="#"><img src="https://cdn.iconscout.com/icon/free/png-256/youtube-1851563-1569283.png" alt="youtube"></a>
+                </div>
+            </div>
+        </div>
+        <div class="payments">
+            <div class="payments_images">
+            </div>
+            <div class="footer_info">
+                <p>Arkadiusz Dampc, Damian Gorski, Julia Korzeniewska, Andrzej Milewczyk, Mariusz Gwara</p>
+                <br>
+                <p>x-kom 2024</p>
+            </div>
+        </div>
     </div>
     </footer>
 </template>
+
+<style lang="scss" scoped>
+@import './footer.scss';
+</style>
