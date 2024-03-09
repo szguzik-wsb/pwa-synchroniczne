37fabd81a18c30cacbfca94fc20af73446cf81a8
Mariusz Gwara <mariogwara@gmail.com>
2024-03-09 12:13:41 +0100

    new carrousel
    Karuzela z produktami promocyjnymi i opisem, 3 produkty na slajdzie.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
     <TopNav/>
     <Carrousel />
+<hr>
+<p style="font-weight: bold; margin-left: 20px;">Promocje</p>
+<br>
+<br>
+    <PromoCarrousel/>
       <slot/>
     <Footer/>
 </template>
